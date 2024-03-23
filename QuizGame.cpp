7151a4575a357d7d0dd665667bfebfe0e11eb372
Seweryn Skrzypik <seweryn.skrzypik@gmail.com>
2024-03-23 19:26:54 +0100

    addded input parsing

--- QuizGame.cpp
+++ QuizGame.cpp
@@ -5,11 +5,17 @@
 #include "QuizGame.h"
 #include <iostream>
 #include <format>
+#include <algorithm>
 
 using namespace std;
 
+char readInputChar();
+
 static string CORRECT_ANSWER = "Gratulacje, zdobywasz 1 punkt";
 static string INCORRECT_ANSWER = "Zla odpowiedz, 0 punktow";
+
+static string NOT_ALLOWED_CHAR = "Uzyto niedozwolonego znaku, dozwolone [a-z]";
+static string TO_MANY_CHARS = "Odpowiedz moze zawierac tylko jeden znak";
 
 static string QUESTIONS_FILE = "C:\\Users\\Lenovo\\CLionProjects\\untitled\\questions.json";
 
@@ -21,8 +27,7 @@ void QuizGame::start() {
 		Question question = this->questionManager.nextQuestion();
 		question.print();
 
-		char answer;
-		cin >> answer;
+		char answer = readInputChar();
 
 		bool correct = answer == question.getAnswer();
 		string message = correct ? CORRECT_ANSWER : INCORRECT_ANSWER;
@@ -36,4 +41,25 @@ void QuizGame::start() {
 	//nie wiem czemu, ale nie moge zapisac ponizszego tekstu jako stala u gory
 	int questionsAmount = this->questionManager.getQuestions().size();
 	cout << format("Ukonczyles test z wynikiem: {}/{}", this->points, questionsAmount);
+}
+
+
+char readInputChar() {
+	string input;
+	while (true) {
+		cin >> input;
+		transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+		if (input.length() > 1) {
+			cout << TO_MANY_CHARS << endl;
+			continue;
+		}
+
+		if(!isalpha(input[0])) {
+			cout << NOT_ALLOWED_CHAR << endl;
+			continue;
+		}
+
+		return input[0];
+	}
 }
